ab50c85120b27eb75468e850563011381871f569
pelanguero <pelanguero@gmail.com>
2021-12-20 23:33:22 -0500

    Primera Animacion

---
+++ src/components/banner.js
@@ -0,0 +1,9 @@
+import * as React from "react"
+import "../style/banner.css"
+class Banner extends React.Component {
+    render() {
+      return (<div id="contbase"><div id="cont"><div id="red"><h1>Argodevs</h1></div><div id="white"></div></div></div>);
+    }
+  }
+
+export default Banner
